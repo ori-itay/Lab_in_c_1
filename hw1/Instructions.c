93a513c096542d44f4f5c2acad14a1366d2c5285
itaylevi1 <39258073+itaylevi1@users.noreply.github.com>
2019-03-08 12:54:53 +0200

    instructions handler updated.
    before any run check.

--- hw1/Instructions.c
+++ hw1/Instructions.c
@@ -3,54 +3,162 @@
 #include "Instructions.h"
 #include "LinkedList.h"
 
-int length(char* string){
-	int length=0;
-	while (string[length]!='\0'){
+
+//private functions headers
+int length(char* string);
+void toLowerCase(char line[]);
+void parse(char command[MAXLINELEN], char parsedCommand[MAXWORDSINCOMMAND][MAXWORDLEN]);
+int getCommandNum(char parsedCommand[MAXWORDSINCOMMAND][MAXWORDLEN]);
+int execute(int commandNum, int i, int j);
+
+
+
+int fetch_and_exec_cmd() {
+	int command_num = -1;
+	char command[MAXLINELEN] = { '\0' };
+	char parsedCommand[MAXWORDSINCOMMAND][MAXWORDLEN] = { { '\0' } };
+
+	if (fgets(command, MAXLINELEN, stdin) == NULL) {
+		return 0;
+	}
+	toLowerCase(command);
+	parse(command, parsedCommand);
+	command_num = getCommandNum(parsedCommand);
+	execute(command_num, atoi(parsedCommand[1]), atoi(parsedCommand[2]));
+
+	return 1;
+}
+
+
+int length(char* string) {
+	int length = 0;
+	while (string[length] != '\0') {
 		length++;
 	}
 	return length;
 }
 
-void toLowerCase(char line[]){
+void toLowerCase(char line[]) {
 	int len = length(line);
-	for (int i=0 ; i < len ; i++){
+	for (int i = 0; i < len; i++) {
 		char letter = line[i];
 		if ((letter < 123 && letter > 96) /* if a lowercase or a valid number or a underscore */
-				|| (letter < 58 && letter > 47) || letter==95 ){
+			|| (letter < 58 && letter > 47) || letter == 95) {
 			continue;
 		}
 		else {
-			line[i]=line[i]+32; /* from Uppercase to Lowercase */
+			line[i] = line[i] + 32; /* from Uppercase to Lowercase */
 		}
 	}
 }
 
-void get_command(char line[]){
 
-}
-
-void fetch(){
-	char command[SIZE]={'\0'};
-	get_command(command);
-	toLowerCase(command);
-
-
-
-}
-
-int execute(){
-
-	return 0;
-}
-
-int fetch_and_exec_cmd(){
-	fetch();
-	execute();
-
-	return 0;
-}
-
-int Exit(){
+int Exit() {
 	free_list();
 	return 1;
+}
+
+
+
+/** parse
+ * -------
+ * Parses a full line of command from user (up to MAXLINELEN chars)
+ * to known commands.
+ *
+ * @param char command[] - A string which is user's 'raw' input command.
+ * @param char parsedCommand[][] - Array of MAXWORDSINCOMMAND strings, which is the user's command parsed into words/numbers.
+ *
+ * @return - void.
+ */
+void parse(char command[MAXLINELEN], char parsedCommand[MAXWORDSINCOMMAND][MAXWORDLEN])
+{
+	int i = 0, letter = 0, word_num = 0;
+
+	for (i = 0; i < MAXLINELEN; i++) {
+		if (command[i] == '\r' || command[i] == '\n' || command[i] == EOF) { // end of command
+
+			break;
+		}
+		else if (command[i] != ' ' || command[i] != '\t') { //curr char is part of a word
+			parsedCommand[word_num][letter++] = command[i];
+		}
+		else { // word ended
+			parsedCommand[word_num][letter++] = '\0'; //end of string char
+			letter = 0; // initialize letter index
+			word_num++;
+		}
+	}
+	return;
+}
+
+
+/** getCommandNum
+ * ----------------
+ * Determines the number of command which is currently in parsedCommand.
+ *
+ * @param char parsedCommand[][] - Array of MAXWORDSINCOMMAND strings, which is the user's command parsed into words/numbers.
+ *
+ * @return - int: The number of the command represented in parsedCommand:
+ * -1: invalid command, 1: add_end i , 2: add_start i , 3: add_after i j, 4: index,
+ * 5: del index , 6: print, 7: exit.
+ */
+int getCommandNum(char parsedCommand[MAXWORDSINCOMMAND][MAXWORDLEN]) {
+	if (strcmp(parsedCommand[0], "add_end") == 0 && strcmp(parsedCommand[1], "") != 0)
+		return 1;
+	if (strcmp(parsedCommand[0], "add_start") == 0 && strcmp(parsedCommand[1], "") != 0)
+		return 2;
+	if (strcmp(parsedCommand[0], "add_after") == 0 && strcmp(parsedCommand[1], "") != 0 && strcmp(parsedCommand[2], "") != 0)
+		return 3;
+	if (strcmp(parsedCommand[0], "index") == 0)
+		return 4;
+	if (strcmp(parsedCommand[0], "del") == 0 && strcmp(parsedCommand[1], "index") == 0)
+		return 5;
+	if (strcmp(parsedCommand[0], "print") == 0)
+		return 6;
+	if (strcmp(parsedCommand[0], "exit") == 0)
+		return 7;
+	return -1;/*no valid command*/
+}
+
+
+/** execute:
+ * ------------
+ * This function executing the proper function according to the passed commandNum.
+ *
+ * @param int commandNum - The number (index) of the command to be executed.
+ * @param char parsedCommand[][] - Array of MAXWORDSINCOMMAND strings, which is the user's command parsed into words/numbers.
+ *
+ * @return: void.
+ */
+int execute(int commandNum, int i, int j) {
+	switch (commandNum) {
+	case -1:
+		printf("ERROR: invalid command\n");
+		break;
+	case 0: /*Blank line, ignore this 'command'*/
+		break;
+	case 1:
+		Add_end(i);
+		break;
+	case 2:
+		Add_start(i);
+		break;
+	case 3:
+		Add_after(i, j);
+		break;
+	case 4:
+		Index_of(i);
+		break;
+	case 5:
+		Delete(i);
+		break;
+	case 6:
+		Print_list();
+		break;
+	case 7:
+		Exit();
+		break;
+	}
+
+	return 0;
 }
--- hw1/Instructions.h
+++ hw1/Instructions.h
@@ -1,10 +1,15 @@
 #ifndef INSTRUCTIONS_H_
 #define INSTRUCTIONS_H_
 
-#define SIZE 100
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINELEN 100
+#define MAXWORDLEN 10
+#define MAXWORDSINCOMMAND 3
 
 int fetch_and_exec_cmd();
-
 
 
 #endif /* INSTRUCTIONS_H_ */
