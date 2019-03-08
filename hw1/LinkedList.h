50274170f34a5991e99a45878fd3cbbbfcb05177
Origithub <39296766+Origithub@users.noreply.github.com>
2019-03-08 15:48:31 +0200

    bug in add_after

--- hw1/LinkedList.h
+++ hw1/LinkedList.h
@@ -2,16 +2,26 @@
 #define LINKEDLIST_H_
 
 
-struct Node{
+struct Node {
 	struct Node* next;
 	int data;
 };
 
-typedef struct{
+typedef struct {
 	struct Node* head;
 }LinkedList;
 
+
+
 void free_list();
+int Add_end(int data);
+int Add_start(int data);
+int Add_after(int insert_after_data_loc, int data_loc);
+int Index_of(int data);
+int Delete(int index);
+int Print_list();
+
+
 
 
 #endif /* LINKEDLIST_H_ */
