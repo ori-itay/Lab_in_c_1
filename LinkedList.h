b0408127c5703b1898daf64adad84282b62a9205
itaylevi1 <39258073+itaylevi1@users.noreply.github.com>
2019-03-08 15:09:10 +0200

    added impl to our strcmp

--- LinkedList.h
+++ LinkedList.h
@@ -11,7 +11,17 @@ typedef struct {
 	struct Node* head;
 }LinkedList;
 
+
+
 void free_list();
+int Add_end(int data);
+int Add_start(int data);
+int Add_after(int insert_after_data_loc, int data_loc);
+int Index_of(int data);
+int Delete(int index);
+int Print_list();
+
+
 
 
 #endif /* LINKEDLIST_H_ */
